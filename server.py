3cce5c1f6c034c4e7996ff99020e27739ba3ca29
Manu S <ms.manu.856@gmail.com>
2023-02-13 11:24:15 +0530

    Network-Chat part 1

--- server.py
+++ server.py
@@ -1,5 +1,22 @@
 import socket
+from tkinter import *
 
+root = Tk()
+
+
+def send(listbox, entry):
+    message = entry.get()
+    listbox.insert('end', message)
+    entry.delete(0, END)
+
+
+entry = Entry()
+entry.pack(side=BOTTOM)
+listbox = Listbox(root)
+listbox.pack()
+button = Button(root, text='Send', command=lambda :send(listbox, entry))
+button.pack()
+root.mainloop()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST_NAME = socket.gethostname()
 PORT = 12345
